4e403a15206b42e60188c29326bd68c75671d3f7
Mikkel Ricky <rimi@aarhus.dk>
2024-08-23 16:44:24 +0200

    Drupal 11 support

--- src/Form/AccessibilityAdminForm.php
+++ src/Form/AccessibilityAdminForm.php
@@ -10,12 +10,14 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class AccessibilityAdminForm extends ConfigFormBase {
 
+  public const CONFIG_NAME = 'web_accessibility_statement.settings';
+
   /**
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
     return [
-      'web_accessibility_statement.accessibilityadmin',
+      static::CONFIG_NAME,
     ];
   }
 
@@ -30,15 +32,18 @@ class AccessibilityAdminForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('web_accessibility_statement.accessibilityadmin');
-    $form['web_accessibility_statement_url'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Accessibility statement absolute url'),
-      '#description' => $this->t('Add the absolute url to the accessibility report.'),
-      '#maxlength' => 128,
+    $config = $this->config(self::CONFIG_NAME);
+
+    $form['url'] = [
+      '#type' => 'url',
+      '#title' => $this->t('Web accessibility statement URL'),
+      '#description' => $this->t('Absolute URL to the web accessibility statement.'),
+      '#maxlength' => 256,
       '#size' => 64,
-      '#default_value' => $config->get('web_accessibility_statement_url'),
+      '#required' => TRUE,
+      '#default_value' => $config->get('url'),
     ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -48,8 +53,8 @@ class AccessibilityAdminForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    $this->config('web_accessibility_statement.accessibilityadmin')
-      ->set('web_accessibility_statement_url', $form_state->getValue('web_accessibility_statement_url'))
+    $this->config(self::CONFIG_NAME)
+      ->set('url', $form_state->getValue('url'))
       ->save();
   }
 
